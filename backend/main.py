e423c9982b3696fc1be190216105e6863a622438
Varshitha Sri Sai <nvarshithasrisai11@gmail.com>
2026-02-18 20:48:47 +0530

    small changes in main.py

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
 
 from backend.routes import oauth_routes, workflow_routes, github_routes
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="GitHub Workflow Automator",
     version="1.0.0",
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(oauth_routes.router, prefix="/oauth", tags=["OAuth"])
